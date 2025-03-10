439a2405b17393fdbbd63a68355021a2faa6ce65
Rocksri <srinathanand1979@gmail.com>
2025-03-10 13:10:38 +0530

    work on cart

--- src/products/productmap.jsx
+++ src/products/productmap.jsx
@@ -64,44 +64,37 @@ export default function OnClickCartAdds({ products }) {
 
 
     return (
-        <div>
-            {/* Cart Items Display - Only when isCartOpen is true */}
-            {isCartOpen && (
+        isCartOpen && (
+            <div className="w-[20%]">
                 <div className="cart-container flex">
                     {cart.map((product, index) => (
                         <div
                             key={index}
-                            className="productCard justify-items-center p-[5%] flex"
-                            style={{
-                                height: cart.length < 7 ? "60%" : "auto",
-                            }}
+                            className="productCard justify-items-center p-[5%] flex flex-col"
                         >
                             <img
                                 src={product.image}
                                 alt={product.title}
-                                className="h-[60%]"
+                                style={{ height: "auto" }}
                             />
                             <p
-                                className={`font-medium product_title_${
+                                className={`font-medium text-2xl product_title_${
                                     index + 1
                                 }`}
                             >
                                 {product.title}
                             </p>
-                            <p className="text-xl font-bold">
+                            <p className="text-2xl font-bold">
                                 ${product.price}
                             </p>
-                            <div className="flex font-semibold">
-                                <span id={`product_title_cart_${index + 1}`}>
-                                    Add to Cart
-                                </span>
-                                <span>Buy Now</span>
+                            <div className="flex font-semibold text-2xl">
+                                Buy Now
                             </div>
                         </div>
                     ))}
                 </div>
-            )}
-        </div>
+            </div>
+        )
     );
 }
 
